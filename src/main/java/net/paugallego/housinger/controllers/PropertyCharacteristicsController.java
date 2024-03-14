3f9600b507a7559509745b62d97b548b98c12bfe
Pau Gallego <paugf2004@gmail.com>
2024-03-14 12:59:46 +0100

    get property by characteristics

--- src/main/java/net/paugallego/housinger/controllers/PropertyCharacteristicsController.java
+++ src/main/java/net/paugallego/housinger/controllers/PropertyCharacteristicsController.java
@@ -1,6 +1,7 @@
 package net.paugallego.housinger.controllers;
 
 import net.paugallego.housinger.exceptions.ApiErrorEnum;
+import net.paugallego.housinger.model.dto.FindPropertyByCharacteristicsDTO;
 import net.paugallego.housinger.model.dto.PropertyCharacteristicsDTO;
 import net.paugallego.housinger.model.dto.PropertyDTO;
 import net.paugallego.housinger.model.dto.PropertyReviewsDTO;
@@ -9,11 +10,9 @@ import net.paugallego.housinger.services.crud.entity.PropertyReviewsCRUDService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,5 +35,30 @@ public class PropertyCharacteristicsController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiErrorEnum.INDETERMINATE_ERROR);
         }
     }
+
+    @GetMapping("/get")
+    public ResponseEntity<?> getByDTO(@RequestBody FindPropertyByCharacteristicsDTO dto) {
+        try {
+            List<Long> characteristics = dto.getCharacteristics();
+            List<PropertyCharacteristicsDTO> dtos = new ArrayList<>();
+
+            List<PropertyCharacteristicsDTO> properties = service.findById(characteristics.get(0));
+
+            for (int i = 1; i < characteristics.size(); i++) {
+                Long characteristicId = characteristics.get(i);
+                List<PropertyCharacteristicsDTO> characteristicProperties = service.findById(characteristicId);
+
+                properties.retainAll(characteristicProperties);
+            }
+
+            dtos.addAll(properties);
+
+            return ResponseEntity.status(HttpStatus.OK).body(dtos);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiErrorEnum.INDETERMINATE_ERROR);
+        }
+    }
+
 
 }
