1d4d9fa69ad6fe58f225afc879d471137be04fb4
Pau Gallego <paugf2004@gmail.com>
2024-04-04 12:31:22 +0200

    fix cors

--- src/main/java/net/paugallego/housinger/HousingerBackend.java
+++ src/main/java/net/paugallego/housinger/HousingerBackend.java
@@ -2,12 +2,13 @@ package net.paugallego.housinger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "net.paugallego.housinger")
 public class HousingerBackend {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HousingerBackend.class, args);
 	}
-
 }
