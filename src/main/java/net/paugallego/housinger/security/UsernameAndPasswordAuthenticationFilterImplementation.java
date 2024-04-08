0fa6b47b4f20446f0529aa26f48167d863cbc066
PauGallego <paugf2004@gmail.com>
2024-04-08 14:52:09 +0200

    login body added

--- src/main/java/net/paugallego/housinger/security/UsernameAndPasswordAuthenticationFilterImplementation.java
+++ src/main/java/net/paugallego/housinger/security/UsernameAndPasswordAuthenticationFilterImplementation.java
@@ -3,13 +3,20 @@ package net.paugallego.housinger.security;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.paugallego.housinger.configs.SecurityConstants;
+import net.paugallego.housinger.model.database.entities.UserEntity;
+import net.paugallego.housinger.model.database.repositories.UserRepository;
 import net.paugallego.housinger.model.dto.LoginDTO;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
@@ -27,6 +34,7 @@ public class UsernameAndPasswordAuthenticationFilterImplementation extends Usern
         this.authenticationManager = authenticationManager;
         setFilterProcessesUrl(SecurityConstants.SIGN_IN_ENDPOINT);
     }
+
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
@@ -60,8 +68,25 @@ public class UsernameAndPasswordAuthenticationFilterImplementation extends Usern
 
             System.out.println("Authentication successful: " + auth); // Log here
 
+            response.setStatus(HttpServletResponse.SC_OK);
+
+            UserEntity user = (UserEntity) auth.getPrincipal();
+
+            JSONObject json = new JSONObject();
+            json.put("mail", user.getMail());
+            json.put("userId", user.getId());
+            json.put("customerId", user.getCustomerEntity().getId());
+            json.put("name", user.getCustomerEntity().getName());
+            json.put("surname", user.getCustomerEntity().getSurname());
+            json.put("picture", user.getCustomerEntity().getPicture());
+
+
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(json.toString());
+
             return auth;
-        } catch (AuthenticationException e) {
+        } catch (AuthenticationException | IOException e) {
             throw new RuntimeException(e);
         }
     }
