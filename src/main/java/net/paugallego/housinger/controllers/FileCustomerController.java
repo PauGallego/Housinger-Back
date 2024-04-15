8c7a28255710bda96f6c3a9407b6dc1c5c7371d2
Pau Gallego <paugf2004@gmail.com>
2024-04-15 09:29:38 +0200

    property images endpoint

--- src/main/java/net/paugallego/housinger/controllers/FileCustomerController.java
+++ src/main/java/net/paugallego/housinger/controllers/FileCustomerController.java
@@ -1,9 +1,12 @@
 package net.paugallego.housinger.controllers;
 
+import net.paugallego.housinger.model.database.entities.PropertyEntity;
 import net.paugallego.housinger.model.database.entities.UserEntity;
+import net.paugallego.housinger.model.database.repositories.PropertyRepository;
 import net.paugallego.housinger.model.dto.LoginDTO;
 import net.paugallego.housinger.model.dto.RegisterDTO;
 import net.paugallego.housinger.services.crud.StorageService;
+import net.paugallego.housinger.services.crud.entity.PropertyCRUDService;
 import net.paugallego.housinger.services.crud.entity.UserCRUDService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -13,6 +16,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/v1/fileCustomer")
@@ -20,6 +25,10 @@ public class FileCustomerController {
 
     @Autowired
     private StorageService storageService;
+
+    @Autowired
+    private PropertyRepository propertyRepository;
+
 
     @PostMapping("/upload/{customerId}")
     public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file, @PathVariable Long customerId) {
@@ -50,6 +59,25 @@ public class FileCustomerController {
             }
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error downloading file: " + e.getMessage());
+        }
+    }
+
+    @PostMapping("/uploadProperty/{propertyId}")
+    public ResponseEntity<?> uploadPropertyPhotos(@RequestParam("files") List<MultipartFile> files, @PathVariable Long propertyId) {
+        try {
+
+
+            PropertyEntity property = propertyRepository.findById(propertyId).orElse(null);
+            if (property == null) {
+                return ResponseEntity.notFound().build();
+            }
+
+           String response = storageService.uploadImageToFileSystemProperty(files, propertyId);
+
+
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error uploading property photos: " + e.getMessage());
         }
     }
 
--- src/main/java/net/paugallego/housinger/services/crud/StorageService.java
+++ src/main/java/net/paugallego/housinger/services/crud/StorageService.java
@@ -1,6 +1,8 @@
 package net.paugallego.housinger.services.crud;
 
 import net.paugallego.housinger.model.database.entities.CustomerEntity;
+import net.paugallego.housinger.model.database.entities.PropertyEntity;
+import net.paugallego.housinger.model.database.repositories.PropertyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,6 +13,7 @@ import net.paugallego.housinger.model.database.repositories.CustomerRepository;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -20,6 +23,10 @@ public class StorageService {
 
     @Autowired
     private CustomerRepository customerRepository;
+
+
+    @Autowired
+    private PropertyRepository propertyRepository;
 
     private final String FOLDER_PATH = "/home/paugallego/Pictures/";
 
@@ -45,6 +52,40 @@ public class StorageService {
             return "File uploaded successfully: " + filePath;
         } else {
             throw new IllegalArgumentException("Customer with ID " + customerId + " not found");
+        }
+
+
+
+    }
+
+    public String uploadImageToFileSystemProperty(List<MultipartFile> files, Long propertyId) throws IOException {
+        Optional<PropertyEntity> propertyEntity = propertyRepository.findById(propertyId);
+        if (propertyEntity.isPresent()) {
+            List<String> fileNames = new ArrayList<>();
+
+            for (MultipartFile file : files) {
+                String originalFileName = file.getOriginalFilename();
+                String fileName = UUID.randomUUID().toString() + "_" + originalFileName;
+                String filePath = FOLDER_PATH + fileName;
+
+                File directory = new File(FOLDER_PATH);
+                if (!directory.exists()) {
+                    directory.mkdirs();
+                }
+
+                File dest = new File(filePath);
+                file.transferTo(dest);
+
+                fileNames.add(fileName);
+            }
+
+            PropertyEntity property = propertyEntity.get();
+            property.setFotos(fileNames);
+            propertyRepository.save(property);
+
+            return "Files uploaded successfully";
+        } else {
+            throw new IllegalArgumentException("Property with ID " + propertyId + " not found");
         }
     }
 
