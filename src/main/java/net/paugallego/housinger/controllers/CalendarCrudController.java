cee695626dd1f7f1d1002f10b0474f70ccd5d2c9
Pau Gallego <paugf2004@gmail.com>
2024-04-29 13:22:01 +0200

    deleted chats

--- src/main/java/net/paugallego/housinger/controllers/CalendarCrudController.java
+++ src/main/java/net/paugallego/housinger/controllers/CalendarCrudController.java
@@ -1,15 +1,10 @@
 package net.paugallego.housinger.controllers;
 
 import net.paugallego.housinger.model.database.entities.CalendarEntity;
-import net.paugallego.housinger.model.database.entities.ChatEntity;
 import net.paugallego.housinger.model.database.repositories.CalendarRepository;
-import net.paugallego.housinger.model.database.repositories.ChatRepository;
 import net.paugallego.housinger.model.dto.CalendarDTO;
-import net.paugallego.housinger.model.dto.ChatDTO;
 import net.paugallego.housinger.services.crud.dto.CalendarDTOConverter;
-import net.paugallego.housinger.services.crud.dto.ChatDTOConverter;
 import net.paugallego.housinger.services.crud.entity.CalendarCRUDService;
-import net.paugallego.housinger.services.crud.entity.ChatCRUDService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
